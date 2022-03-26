bf683a7ccd22695389bc44f153e022ab7deff34c
memotiur <memotiur@gmail.com>
2022-03-26 19:57:48 +0600

    ..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,8 +82,7 @@
                                                 </div>
                                             </div>
                                             <h2 class="hp-main-title">Saiful Islam</h2>
-                                            <p>Hi, I'm Saiful Islam,
-                                                a software engineer specialized in
+                                            <p>Hi, I'm a software engineer specialized in
                                                 both front-end and back-end web development.
                                                 I am an allround web developer. I love structure and order and I also
                                                 stand for quality. I love spending time on fixing little details and
@@ -388,63 +387,99 @@
                                             <li class="active"><a class="filter btn btn-sm btn-link"
                                                                   data-group="category_all">All</a></li>
                                             <li><a class="filter btn btn-sm btn-link"
-                                                   data-group="category_media">Media</a></li>
-                                            <li><a class="filter btn btn-sm btn-link" data-group="category_mockups">Mockups</a>
+                                                   data-group="category_media">Ecommerce</a></li>
+                                            <li><a class="filter btn btn-sm btn-link"
+                                                   data-group="category_mockups">Lms</a>
                                             </li>
-                                            <li><a class="filter btn btn-sm btn-link" data-group="category_soundcloud">SoundCloud</a>
+                                            <li><a class="filter btn btn-sm btn-link" data-group="category_soundcloud">Tour
+                                                    Guide</a>
                                             </li>
                                             <li><a class="filter btn btn-sm btn-link"
-                                                   data-group="category_vimeo-videos">Vimeo Videos</a></li>
-                                            <li><a class="filter btn btn-sm btn-link"
-                                                   data-group="category_youtube-videos">YouTube Videos</a></li>
+                                                   data-group="category_vimeo-videos">Website</a></li>
+
                                         </ul>
                                         <div class="portfolio-grid three-columns">
                                             <figure class="item lbaudio"
-                                                    data-groups='["category_all", "category_soundcloud"]'>
-                                                <div class="portfolio-item-img"><img src="images/portfolio/1.jpg"
-                                                                                     alt="SoundCloud Audio" title=""/>
-                                                    <a href="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/221650664&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"
-                                                       class="lightbox mfp-iframe" title="SoundCloud Audio"></a></div>
-                                                <i class="fa fa-volume-up"></i> <h4 class="name">SoundCloud Audio</h4>
-                                                <span class="category">SoundCloud</span></figure>
-                                            <figure class="item standard"
-                                                    data-groups='["category_all", "category_media"]'>
-                                                <div class="portfolio-item-img"><img src="images/portfolio/2.jpg"
-                                                                                     alt="Media Project 2" title=""/> <a
-                                                        href="portfolio-1.html" class="ajax-page-load"></a></div>
-                                                <i class="far fa-file-alt"></i> <h4 class="name">Media Project 2</h4>
-                                                <span class="category">Media</span></figure>
-                                            <figure class="item lbvideo"
-                                                    data-groups='["category_all", "category_vimeo-videos"]'>
-                                                <div class="portfolio-item-img"><img src="images/portfolio/3.jpg"
-                                                                                     alt="Vimeo Video 1" title=""/> <a
-                                                        href="https://player.vimeo.com/video/158284739"
-                                                        class="lightbox mfp-iframe" title="Vimeo Video 1"></a></div>
-                                                <i class="fas fa-video"></i> <h4 class="name">Vimeo Video 1</h4> <span
-                                                    class="category">Vimeo Videos</span></figure>
-                                            <figure class="item standard"
-                                                    data-groups='["category_all", "category_media"]'>
-                                                <div class="portfolio-item-img"><img src="images/portfolio/4.jpg"
-                                                                                     alt="Media Project 1" title=""/> <a
-                                                        href="portfolio-1.html" class="ajax-page-load"></a></div>
-                                                <i class="far fa-file-alt"></i> <h4 class="name">Media Project 1</h4>
-                                                <span class="category">Media</span></figure>
-                                            <figure class="item lbimage"
                                                     data-groups='["category_all", "category_mockups"]'>
-                                                <div class="portfolio-item-img"><img src="images/portfolio/5.jpg"
-                                                                                     alt="Mockup Design 1" title=""/> <a
+                                                <div class="portfolio-item-img"><img src="/images/quizcarnival.png"
+                                                                                     alt="Learning Management System"
+                                                                                     title=""/>
+                                                    <a href="/images/quizcarnival.png"
+                                                       class="lightbox mfp-iframe" title="Quiz Carnival"></a></div>
+                                                <i class="fa fa-volume-up"></i> <h4 class="name">Learning Management
+                                                    System</h4>
+                                                <span class="category">
+                                                    <a target="_blank"
+                                                       href="https://quizcarnival.com/">Quiz Carnival</a>
+                                                </span></figure>
+                                            <figure class="item standard"
+                                                    data-groups='["category_all", "category_soundcloud"]'>
+                                                <div class="portfolio-item-img"><img src="/images/ruchi.png"
+                                                                                     alt="Media Project 2" title=""/> <a
+                                                        href="/images/ruchi.png" class="ajax-page-load"></a></div>
+                                                <i class="far fa-file-alt"></i> <h4 class="name">
+                                                    <a target="_blank" href="https://ruchiexplorelimitless.com/">Ruchi
+                                                        Explore Limitless</a></h4>
+                                                <span class="category">
+                                                    Tour Guide
+                                                </span></figure>
+                                            <figure class="item lbvideo"
+                                                    data-groups='["category_all", "category_media"]'>
+                                                <div class="portfolio-item-img"><img src="/images/martvenue.png"
+                                                                                     alt="MartVenue" title=""/> <a
+                                                        href="/images/martvenue.png"
+                                                        class="lightbox mfp-iframe" title="Vimeo Video 1"></a></div>
+                                                <i class="fas fa-video"></i> <h4 class="name"><a href="https://martvenue.com/" target="_blank">
+                                                        MartVenue
+                                                    </a> </h4> <span
+                                                    class="category">Ecommerce</span></figure>
+                                            <figure class="item standard"
+                                                    data-groups='["category_all", "category_media"]'>
+                                                <div class="portfolio-item-img"><img src="/images/firebox.png"
+                                                                                     alt="Firebox" title=""/> <a
+                                                        href="/images/firebox.png" class="ajax-page-load"></a></div>
+                                                <i class="far fa-file-alt"></i> <h4 class="name">Firebox</h4>
+                                                <span class="category">Ecommerce</span></figure>
+                                            <figure class="item lbimage"
+                                                    data-groups='["category_all", "category_media"]'>
+                                                <div class="portfolio-item-img"><img src="/images/kenarhat.png"
+                                                                                     alt="Kenarhat" title=""/> <a
                                                         class="lightbox" title="Mockup Design 1"
                                                         href="images/portfolio/5.jpg"></a></div>
-                                                <i class="far fa-image"></i> <h4 class="name">Mockup Design 1</h4> <span
-                                                    class="category">Mockups</span></figure>
+                                                <i class="far fa-image"></i> <h4 class="name">Kenarhat</h4> <span
+                                                    class="category">Ecommerce</span></figure>
                                             <figure class="item lbvideo"
-                                                    data-groups='["category_all", "category_youtube-videos"]'>
-                                                <div class="portfolio-item-img"><img src="images/portfolio/6.jpg"
-                                                                                     alt="YouTube Video 1" title=""/> <a
-                                                        href="https://www.youtube.com/embed/bg0gv2YpIok"
-                                                        class="lightbox mfp-iframe" title="YouTube Video 1"></a></div>
-                                                <i class="fas fa-video"></i> <h4 class="name">YouTube Video 1</h4> <span
-                                                    class="category">YouTube Videos</span></figure>
+                                                    data-groups='["category_all", "category_vimeo-videos"]'>
+                                                <div class="portfolio-item-img"><img src="/images/zsc.png"
+                                                                                     alt="ZSC Enterprise" title=""/> <a
+                                                        href="/images/zsc.png"
+                                                        class="lightbox mfp-iframe" title="ZSC Enterprise"></a></div>
+                                                <i class="fas fa-video"></i> <h4 class="name">ZSC Enterprise</h4> <span
+                                                    class="category">Website</span></figure>
+                                            <figure class="item lbvideo"
+                                                    data-groups='["category_all", "category_vimeo-videos"]'>
+                                                <div class="portfolio-item-img"><img src="/images/military.png"
+                                                                                     alt="Bangabandhu Military Museum" title=""/> <a
+                                                        href="/images/military.png"
+                                                        class="lightbox mfp-iframe" title="Bangabandhu Military Museum"></a></div>
+                                                <i class="fas fa-video"></i> <h4 class="name">Bangabandhu Military Museum</h4> <span
+                                                    class="category">Website</span></figure>
+                                            <figure class="item lbvideo"
+                                                    data-groups='["category_all", "category_vimeo-videos"]'>
+                                                <div class="portfolio-item-img"><img src="/images/eventtech.png"
+                                                                                     alt="Eventtech Bd" title=""/> <a
+                                                        href="/images/eventtech.png"
+                                                        class="lightbox mfp-iframe" title="Eventtech Bd"></a></div>
+                                                <i class="fas fa-video"></i> <h4 class="name">Eventtech Bd</h4> <span
+                                                    class="category">Website</span></figure>
+                                            <figure class="item lbvideo"
+                                                    data-groups='["category_all", "category_vimeo-videos"]'>
+                                                <div class="portfolio-item-img"><img src="/images/kitefest.png"
+                                                                                     alt="Eventtech Bd" title=""/> <a
+                                                        href="/images/kitefest.png"
+                                                        class="lightbox mfp-iframe" title="Kitefest California"></a></div>
+                                                <i class="fas fa-video"></i> <h4 class="name">Kitefest California</h4> <span
+                                                    class="category">Website</span></figure>
                                         </div>
                                     </div>
                                 </div>
